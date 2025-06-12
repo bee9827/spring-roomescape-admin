132bdd07ddd310f866c2ce79e23cbe80d4b0b79b
bee9827 <ehfrhfo9494@naver.com>
2025-06-13 01:20:54 +0900

    chore: 세팅

---
+++ src/test/java/roomescape/admin/RoomescapeApplicationTests.java
@@ -0,0 +1,13 @@
+package roomescape.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomescapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
